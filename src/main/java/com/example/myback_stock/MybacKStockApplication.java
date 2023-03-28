bede63e7f01dde61f5177c86c3c718b237400efa
ANOUSSI <anoussikamla@gmail.com>
2023-03-28 13:40:21 +0100

    first commit

---
+++ src/main/java/com/example/myback_stock/MybacKStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.myback_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybacKStockApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MybacKStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myback_stock/MybacKStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myback_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybacKStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
